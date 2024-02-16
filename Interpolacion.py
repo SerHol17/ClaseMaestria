cf2a43101df7dfc7aeda08b7cd08d78a34168ecb
SerHol17 <136134254+SerHol17@users.noreply.github.com>
2024-02-15 19:06:37 -0500

    Update Interpolacion.py
    Se agrega el angulo b y se completa la interpolación con el angulo a

--- Interpolacion.py
+++ Interpolacion.py
@@ -16,3 +16,9 @@ L1L2= np.array([2.975,2.950,2.9,2.825,2.725,2.625,2.500,2.350,2.200])
 a=30
 b=55
 print("El valor interpolado para",a,"grados en x/L2 es:",interNumpy(x,xL2,a))
+print("El valor interpolado para",a,"grados en L3/L2 es:",interNumpy(x,L3L2,a))
+print("El valor interpolado para",a,"grados en L1/L2 es:",interNumpy(x,L3L2,a))
+
+print("El valor interpolado para",b,"grados en x/L2 es:",interNumpy(x,xL2,b))
+print("El valor interpolado para",b,"grados en L3/L2 es:",interNumpy(x,L3L2,b))
+print("El valor interpolado para",b,"grados en L1/L2 es:",interNumpy(x,L3L2,b))
