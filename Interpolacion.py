eb743ebc0f1f6231b70c1c7f618e247987c32674
SerHol17 <136134254+SerHol17@users.noreply.github.com>
2024-02-15 19:48:55 -0500

    Update Interpolacion.py
    Codigo completo y funcional

--- Interpolacion.py
+++ Interpolacion.py
@@ -13,7 +13,7 @@ import pandas as pd
 def interNumpy(x,y,x0):
   return np.interp(x0, x, y)
 
-
+print("Interpolación usando Numpy")
 #Cargamos Vectores Fuerza
 x=np.array([20, 40, 60, 80, 100, 120, 140, 160, 180]) #Angulos
 xL2=np.array([0.601, 1.193, 1.763, 2.299, 2.790, 3.238, 3.623, 3.933, 4.181])
@@ -33,6 +33,7 @@ data = {'Grados': [30,55],
         'L1/L2': [interNumpy(x,L1L2,a), interNumpy(x,L3L2,b)],
         'L3/L2': [ interNumpy(x,L3L2,a), interNumpy(x,L1L2,b)]}
 df = pd.DataFrame(data)
+print("Fuerza")
 print(df)
 
 #Cargamos Vectores velocidad
@@ -86,22 +87,30 @@ xL2=np.array([0.601, 1.193, 1.763, 2.299, 2.790, 3.238, 3.623, 3.933, 4.181])
 L3L2=np.array([3.963,3.925,3.850,3.738,3.588,3.438,3.250,3.025,2.800])
 L1L2= np.array([2.975,2.950,2.9,2.825,2.725,2.625,2.500,2.350,2.200])
 
+data = {'Grados': [30,55],
+        'L1': [interLagrange(x,L1L2,a)*(20/interLagrange(x,xL2,a)),interLagrange(x,L1L2,b)*(20/interLagrange(x,xL2,b))],
+        'L2': [20/interLagrange(x,xL2,a),20/interLagrange(x,xL2,b)],
+        'L3': [ interLagrange(x,L3L2,a)*(20/interLagrange(x,xL2,a)), interLagrange(x,L3L2,b)*(20/interLagrange(x,xL2,b))],
+        'x/L2': [ interLagrange(x,xL2,a), interLagrange(x,xL2,b)],
+        'L1/L2': [interLagrange(x,L1L2,a), interLagrange(x,L3L2,b)],
+        'L3/L2': [ interLagrange(x,L3L2,a), interLagrange(x,L1L2,b)]}
+df = pd.DataFrame(data)
+print("Fuerza")
+print(df)
 
-print("El valor interpolado para L1",a,"en fuerza es:", interLagrange(x,L1L2,a)*(20/interLagrange(x,xL2,a)))
-print("El valor interpolado para L2",a,"en fuerza es:", 20/interLagrange(x,xL2,a))
-print("El valor interpolado para L3",a,"en fuerza es:", interLagrange(x,L3L2,a)*(20/interLagrange(x,xL2,a)))
 
-print("El valor interpolado para",a,"grados en x/L2 en fuerza es:",interLagrange(x,xL2,a))
-print("El valor interpolado para",a,"grados en L3/L2 en fuerza es:",interLagrange(x,L3L2,a))
-print("El valor interpolado para",a,"grados en L1/L2 en fuerza es:",interLagrange(x,L1L2,a))
+xL2=np.array([0.480, 0.950, 1.411, 1.845, 2.237, 2.600, 2.932,3.232,3.456])
+L3L2=np.array([2.613, 2.575, 2.538, 2.463, 2.350, 2.238, 2.125,2.013,1.863])
+L1L2= np.array([1.045, 1.124, 1.178, 1.229, 1.275, 1.319, 1.347,1.361,1.374])
 
-#Valores 55°
-
-print("El valor interpolado para L1",b,"en fuerza es:", interLagrange(x,L1L2,b)*(20/interLagrange(x,xL2,b)))
-print("El valor interpolado para L2",b,"en fuerza es:", 20/interLagrange(x,xL2,b))
-print("El valor interpolado para L3",b,"en fuerza es:", interLagrange(x,L3L2,b)*(20/interLagrange(x,xL2,b)))
-
-print("El valor interpolado para",b,"grados en x/L2 en fuerza es:",interLagrange(x,xL2,b))
-print("El valor interpolado para",b,"grados en L3/L2 en fuerza es:",interLagrange(x,L3L2,b))
-print("El valor interpolado para",b,"grados en L1/L2 en fuerza es:",interLagrange(x,L1L2,b))
+data = {'Grados': [30,55],
+        'L1': [interLagrange(x,L1L2,a)*(20/interLagrange(x,xL2,a)),interLagrange(x,L1L2,b)*(20/interLagrange(x,xL2,b))],
+        'L2': [20/interLagrange(x,xL2,a),20/interLagrange(x,xL2,b)],
+        'L3': [ interLagrange(x,L3L2,a)*(20/interLagrange(x,xL2,a)), interLagrange(x,L3L2,b)*(20/interLagrange(x,xL2,b))],
+        'x/L2': [ interLagrange(x,xL2,a), interLagrange(x,xL2,b)],
+        'L1/L2': [interLagrange(x,L1L2,a), interLagrange(x,L3L2,b)],
+        'L3/L2': [ interLagrange(x,L3L2,a), interLagrange(x,L1L2,b)]}
+df = pd.DataFrame(data)
+print("Velocidad")
+print(df)
 
