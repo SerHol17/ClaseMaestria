bd58797391684fc6d36021b47ccbeb78721be9a6
SerHol17 <136134254+SerHol17@users.noreply.github.com>
2024-02-15 19:04:33 -0500

    Update Interpolacion.py
    Se optimiza la interpolación usando numpy para la fuerza

--- Interpolacion.py
+++ Interpolacion.py
@@ -1,66 +1,18 @@
-#Interpolación usando Nunmpy
+#Interpolación Usando Numpy
 
-#Fuerza
-
-#20/L2
-
-#30
 import numpy as np
 
-# Vectores dados
-xi = np.array([20, 40, 60, 80, 100, 120, 140, 160, 180])
-fi = np.array([0.601, 1.193, 1.763, 2.299, 2.790, 3.238, 3.623, 3.933, 4.181])
-
-# Valor a interpolar
-x_interpolar = 30
-
-# Realizar la interpolación
-f_interpolar = np.interp(x_interpolar, xi, fi)
-
-print(f"El valor interpolado para xi=30 es: {f_interpolar}")
+def interNumpy(x,y,x0):
+  return np.interp(x0, x, y)
 
 
-# Vectores dados
+#Cargamos Vectores
+x=np.array([20, 40, 60, 80, 100, 120, 140, 160, 180]) #Angulos
+xL2=np.array([0.601, 1.193, 1.763, 2.299, 2.790, 3.238, 3.623, 3.933, 4.181])
+L3L2=np.array([3.963,3.925,3.850,3.738,3.588,3.438,3.250,3.025,2.800])
+L1L2= np.array([2.975,2.950,2.9,2.825,2.725,2.625,2.500,2.350,2.200])
 
-#55
-xi = np.array([20, 40, 60, 80, 100, 120, 140, 160, 180])
-fi = np.array([0.601, 1.193, 1.763, 2.299, 2.790, 3.238, 3.623, 3.933, 4.181])
-
-# Valor a interpolar
-x_interpolar = 55
-
-# Realizar la interpolación
-f_interpolar = np.interp(x_interpolar, xi, fi)
-
-print(f"El valor interpolado para xi=30 es: {f_interpolar}")
-
-
-#L3/L2
-
-#30
-
-xi = np.array([20, 40, 60, 80, 100, 120, 140, 160, 180])
-fi = np.array([3.963,3.925,3.850,3.738,3.588,3.438,3.250,3.025,2.800])
-
-# Valor a interpolar
-x_interpolar = 30
-
-# Realizar la interpolación
-f_interpolar = np.interp(x_interpolar, xi, fi)
-
-print(f"El valor interpolado para xi=30 es: {f_interpolar}")
-
-#55
-
-# Vectores dados
-xi = np.array([20, 40, 60, 80, 100, 120, 140, 160, 180])
-fi = np.array([3.963,3.925,3.850,3.738,3.588,3.438,3.250,3.025,2.800])
-
-# Valor a interpolar
-x_interpolar = 55
-
-# Realizar la interpolación
-f_interpolar = np.interp(x_interpolar, xi, fi)
-
-print(f"El valor interpolado para xi=30 es: {f_interpolar}")
-
+#Angulos a interpolar
+a=30
+b=55
+print("El valor interpolado para",a,"grados en x/L2 es:",interNumpy(x,xL2,a))
